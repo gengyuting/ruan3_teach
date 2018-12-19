83819a9f88ba8632291268030fdee475ae5ca2e9
gengyuting <2662045166@qq.com>
2018-12-19 20:13:23 +0800

    实现了学生信息的分页、模糊查询

--- src/main/java/com/tt/teach/controller/StudentController.java
+++ src/main/java/com/tt/teach/controller/StudentController.java
@@ -6,10 +6,12 @@ import com.tt.teach.utils.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/stu")
@@ -17,12 +19,13 @@ public class StudentController extends BaseController {
     @Resource
     private StudentService studentService;
 
+    //登录的请求
     @RequestMapping("/login")
     //请求：http://localhost:8080/stu/login
     public String login() {
         return "/student/login";
     }
-
+    //判断
     @RequestMapping("/index")
     //请求：http://localhost:8080/stu/index
     public String index() {
@@ -32,12 +35,20 @@ public class StudentController extends BaseController {
         }
         return REDIRECT+"/stu/login";
     }
+    //学生查询的请求
+    @RequestMapping("/student")
+    //请求：http://localhost:8080/stu/student
+    public String student() {
+        return "/student/student";
+    }
 
+    //注销的请求
     @RequestMapping("/logout")
     public String logout(HttpSession session) {
         session.removeAttribute(SESSION_KEY);
         return REDIRECT+"/stu/login";
     }
+
 
     //登录方法
     @RequestMapping(value = "/doLogin",method = RequestMethod.POST)
@@ -55,5 +66,20 @@ public class StudentController extends BaseController {
             return FORWARD+"/stu/index";
         }
          return REDIRECT+"/stu/login";
+    }
+
+    //查询所有学生信息的方法
+    //请求：http://localhost:8080/stu/getStudentList
+    @RequestMapping(value = "/getStudentList",method = RequestMethod.GET)
+    @ResponseBody
+    public Object getStudentList() {
+        List<Student> list = studentService.getStudentList();
+        return list;
+    }
+
+    //删除学生的方法
+    @RequestMapping(value = "/deleteStudent",method = RequestMethod.GET)
+    public String deleteStudent() {
+        return "明见";
     }
 }
--- src/main/java/com/tt/teach/service/StudentService.java
+++ src/main/java/com/tt/teach/service/StudentService.java
@@ -2,6 +2,12 @@ package com.tt.teach.service;
 
 import com.tt.teach.pojo.Student;
 
+import java.util.List;
+
 public interface StudentService {
     Student doLogin(Student student);
+
+    List<Student> getStudentList();
+
+
 }
